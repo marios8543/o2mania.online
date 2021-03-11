857bd1abb0a6bf11b232cab78589adec44117edd
tza <tza@hidden>
2021-03-11 19:24:08 +0200

    safety commit

--- client/js/index.js
+++ client/js/index.js
@@ -5,6 +5,7 @@ const app = new Vue({
         "player-box": PlayerBoxComponent
     },
     data: {
+        resources: RESOURCE_URL,
         roomName: "",
         speed: 0,
         maxSpeed: 5,
@@ -15,13 +16,9 @@ const app = new Vue({
             bpm: 170
         },
         TeamColors : {A: "red", B: "orange", C: "yellow", D: "lime", E: "cyan", F: "blue", G: "purple", H: "brown"},
-        players: [
-            new Player("Test-kun", "https://sh.tzatzikiweeb.moe/mikuspin.gif", 89, "B")
-        ],
-        messages: [
-            new Message("Test-kun", "Fuck joggers")
-        ],
-        resources: RESOURCE_URL
+        Genres:["Ballad", "Rock", "Dance" ,"Techno" ,"Hip-hop" ,"Soul/R&B" ,"Jazz" ,"Funk" ,"Classical" ,"Traditional" ,"Etc"],
+        players: [new Player("Test-kun", "https://sh.tzatzikiweeb.moe/mikuspin.gif", 89, "B")],
+        messages: [new Message("Test-kun", "Fuck joggers")]
     },
     methods: {
 
